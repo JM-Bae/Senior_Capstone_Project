4f5efee260997223a7f1f2da6600a150106d5867
Junho Bae <baej8@unlv.nevada.edu>
2018-11-07 20:08:11 -0800

    Added base project to detect human faces using haarcascade

---
+++ utils/inference.py
@@ -0,0 +1,20 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+#from keras.preprocessing import image
+
+
+def draw_bounding_box(face_coordinates, image_array, color):
+    x, y, w, h = face_coordinates
+    cv2.rectangle(image_array, (x, y), (x + w, y + h), color, 2)
+
+def apply_offsets(face_coordinates, offsets):
+    x, y, width, height = face_coordinates
+    x_off, y_off = offsets
+    return (x - x_off, x + width + x_off, y - y_off, y + height + y_off)
+
+def get_colors(num_classes):
+    colors = plt.cm.hsv(np.linspace(0, 1, num_classes)).tolist()
+    colors = np.asarray(colors) * 255
+    return colors
+
